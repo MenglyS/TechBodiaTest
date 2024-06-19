2fbddb710e2fc33eba0838d3bb0b7c8a8f41977a
menglysovan124 <135255375+menglysovan124@users.noreply.github.com>
2024-06-19 23:42:06 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace TechBodiaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter notification channel here: ");
+            string notfChan = Console.ReadLine();
+
+            bool isFirst = true, isOpenBracketFounded = false;
+            string chan = "";
+            
+
+            for (int i = 0; i < notfChan.Length; i++) {
+                if (notfChan[i] == '[') {
+                    isOpenBracketFounded = true;
+                    chan = "";
+                } else if (notfChan[i] == ']' && isOpenBracketFounded){
+                    isOpenBracketFounded = false;
+
+                    if (chan == "BE" || chan == "FE" || chan == "QA" || chan == "Urgent") {
+
+                        if (isFirst) {
+                            isFirst = false;
+                            
+                            Console.Write($"Receive channels: {chan}");
+                        } else {
+                            Console.Write($", {chan}");
+                        }
+                    }
+
+                } else if (isOpenBracketFounded){
+                    chan += notfChan[i];
+                }
+            }
+
+            if (isFirst) {
+                Console.Write("No valid channel found");
+            }
+        }
+    }
+}
